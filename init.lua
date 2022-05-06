b027f38296f9f8125d7915d14f1976b16ab78f8e
Leon Fuss <hello@leonfuss.me>
2022-05-06 19:11:21 +0200

    added lsp process indicator

--- init.lua
+++ init.lua
@@ -6,14 +6,14 @@ vim.opt.syntax = 'enable'
 require 'plugins'
 
 -- Setup colorscheme
-vim.opt.background = 'dark'   -- or 'light'
+vim.opt.background = 'dark' -- or 'light'
 vim.cmd([[colorscheme gruvbox]])
 
 vim.opt.number = true
 vim.o.relativenumber = true
 vim.opt.completeopt = 'menuone,noinsert,noselect'
 vim.opt.cmdheight = 2
-vim.opt.updatetime = 300 	-- for better diagnostic experience
+vim.opt.updatetime = 300 -- for better diagnostic experience
 vim.opt.hidden = true
 vim.opt.wrap = false
 vim.opt.encoding = 'utf-8'
@@ -36,31 +36,31 @@ vim.opt.smartcase = true
 -- Keymaps
 local map = vim.keymap.set
 
-map('i', 'jk', '<ESC>')            -- better esc
+map('i', 'jk', '<ESC>') -- better esc
 map('i', 'kj', '<ESC>')
-map({'n', 'v'}, 'L', '$')          -- practical vertical navigation
-map({'n', 'v'}, 'H', '^')
-map({'n', 'v'}, 'ö', '{')
-map({'n', 'v'}, 'ä', '}')
+map({ 'n', 'v' }, 'L', '$') -- practical vertical navigation
+map({ 'n', 'v' }, 'H', '^')
+map({ 'n', 'v' }, 'ö', '{')
+map({ 'n', 'v' }, 'ä', '}')
 
-map({'v','n'}, '<C-h>', ':nohlsearch<cr>',{silent = true})
+map({ 'v', 'n' }, '<C-h>', ':nohlsearch<cr>', { silent = true })
 
-map('n', '<leader>w', ':w<cr>',{silent = true})        -- leader write
-map('n', '<Tab>', ':bNext<cr>',{silent = true})
+map('n', '<leader>w', ':w<cr>', { silent = true }) -- leader write
+map('n', '<Tab>', ':bNext<cr>', { silent = true })
 
-map('n', '<left>', ':bp<CR>',{silent = true})
-map('n', '<right>', ':bn<CR>',{silent = true})
+map('n', '<left>', ':bp<CR>', { silent = true })
+map('n', '<right>', ':bn<CR>', { silent = true })
 
 -- Treesitter setup
 require('nvim-treesitter.configs').setup {
-    ensure_installed = {'rust', 'lua'},
+    ensure_installed = { 'rust', 'lua' },
     highlight = {
         enable = true
     }
 }
 
 -- Telescope setup
-require('telescope').setup{
+require('telescope').setup {
     pickers = {
         find_files = {
             theme = 'dropdown',
@@ -69,10 +69,10 @@ require('telescope').setup{
             mappings = {
                 n = {
                     --['cd'] = function(prompt_bufnr)
-                        --local selection = require('telescope.actions.state').get_selected_entry()
-                        --local dir = vim.fn.fnamemodify(selection.path, ':p:h')
-                        --require('telescope.actions').close(prompt_bufn)
-                        --vim.cmd(string.format('silent lcd %s', dir))
+                    --local selection = require('telescope.actions.state').get_selected_entry()
+                    --local dir = vim.fn.fnamemodify(selection.path, ':p:h')
+                    --require('telescope.actions').close(prompt_bufn)
+                    --vim.cmd(string.format('silent lcd %s', dir))
                     --end
                 }
             }
@@ -81,25 +81,22 @@ require('telescope').setup{
             theme = 'dropdown',
         }
     },
-    extensions = {                              -- 'wild  | exact match        | Items that include "wild"
-        fzf = {                                 -- ^music | prefix exact match | Items that start with "music"
-            fuzzy = true,                       -- .mp3$  | suffix exact match | Items that end with ".mp3"
-            override_generic_sorter = true,     -- !      | inverse match      | Match the inverse of the following statement
-            override_file_sorter = true,        -- pipe ('|') acts as OR
-            case_mode = "smart_case",           -- <Space> between two statements acts as AND
+    extensions = { -- 'wild  | exact match        | Items that include "wild"
+        fzf = { -- ^music | prefix exact match | Items that start with "music"
+            fuzzy = true, -- .mp3$  | suffix exact match | Items that end with ".mp3"
+            override_generic_sorter = true, -- !      | inverse match      | Match the inverse of the following statement
+            override_file_sorter = true, -- pipe ('|') acts as OR
+            case_mode = "smart_case", -- <Space> between two statements acts as AND
         }
     }
 }
 
 -- Telescope find_files centered dropdown list
-map('n', '<C-f>', ':Telescope find_files<cr>',{silent = true})
-map('n', '<leader>b', ':Telescope buffers<cr>',{silent = true})
+map('n', '<C-f>', ':Telescope find_files<cr>', { silent = true })
+map('n', '<leader>b', ':Telescope buffers<cr>', { silent = true })
 
-require('telescope').load_extension('fzf')      -- override sorter
+require('telescope').load_extension('fzf') -- override sorter
 
--- Feline setup (statusline)
-
---require('feline-config')
 
 -- CMP setup
 local cmp = require 'cmp'
@@ -111,14 +108,14 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<Tab>'] = cmp.mapping.confirm({select = true})
+        ['<Tab>'] = cmp.mapping.confirm({ select = true })
     },
     sources = cmp.config.sources({
-        { name = 'nvim_lsp'},
-        { name = 'nvim_lsp_signature_help'},
-        { name = 'luasnip'},
-    },{
-        { name = 'buffer'},
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'luasnip' },
+    }, {
+        { name = 'buffer' },
     }),
     experimental = {
         ghost_text = true
@@ -129,7 +126,7 @@ cmp.setup({
 require('nvim-lsp-setup').setup({
     installer = {},
     default_mappings = true,
-    mappings =   {},
+    mappings = {},
     on_attach = function(client, _)
         require('nvim-lsp-setup.utils').format_on_save(client)
     end,
@@ -153,3 +150,8 @@ require('nvim-lsp-setup').setup({
         })
     }
 })
+
+require('fidget').setup({})
+
+-- Feline setup (statusline)
+--require('feline-config')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,26 +1,26 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({'git', 'clone', '--depth' , '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 
 -- little autocommands for packer sync when writing to this buffer
-local plugin_augroup = vim.api.nvim_create_augroup("Plugin.lua", {clear = true})
-vim.api.nvim_create_autocmd({"BufWrite"}, {
-    pattern = {'plugins.lua'},
+local plugin_augroup = vim.api.nvim_create_augroup("Plugin.lua", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWrite" }, {
+    pattern = { 'plugins.lua' },
     callback = function()
-        vim.cmd[[source ~/.config/nvim/lua/plugins.lua]]
+        vim.cmd [[source ~/.config/nvim/lua/plugins.lua]]
         require('packer').sync()
     end,
     group = plugin_augroup
 })
 
-return require('packer').startup({function(use)
--- packer can manage itself
-    use 'wbthomason/packer.nvim'		-- Packer can manage itself
-    use 'ellisonleao/gruvbox.nvim'		-- Gruvbox Theme
+return require('packer').startup({ function(use)
+    -- packer can manage itself
+    use 'wbthomason/packer.nvim' -- Packer can manage itself
+    use 'ellisonleao/gruvbox.nvim' -- Gruvbox Theme
 
-    use 'nvim-lua/plenary.nvim'         -- neat functions for nearly everything
+    use 'nvim-lua/plenary.nvim' -- neat functions for nearly everything
 
     use 'nvim-telescope/telescope.nvim' -- imporving the whole experience
     use {
@@ -33,26 +33,27 @@ return require('packer').startup({function(use)
         run = ':TSUpdate'
     }
 
-    use 'neovim/nvim-lspconfig'         -- LSP realted
+    use 'neovim/nvim-lspconfig' -- LSP realted
     use 'williamboman/nvim-lsp-installer' -- LSP installer
-    use 'junnplus/nvim-lsp-setup'       -- esay lsp setup
+    use 'junnplus/nvim-lsp-setup' -- esay lsp setup
+    use 'j-hui/fidget.nvim' -- nvim-lsp-progress spinner
 
-    use 'hrsh7th/nvim-cmp'              -- Completion
+    use 'hrsh7th/nvim-cmp' -- Completion
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
 
-    use 'saadparwaiz1/cmp_luasnip'      -- snippets :)
+    use 'saadparwaiz1/cmp_luasnip' -- snippets :)
     use 'L3MON4D3/LuaSnip'
 
-    use 'simrat39/rust-tools.nvim'      -- one to rule them all
+    use 'simrat39/rust-tools.nvim' -- one to rule them all
     use 'folke/lua-dev.nvim'
 
     use 'kyazdani42/nvim-web-devicons'
 
     use 'feline-nvim/feline.nvim'
 
-    if  packer_bootstrap then
+    if packer_bootstrap then
         require('packer').sync()
     end
 end,
@@ -60,4 +61,4 @@ config = {
     display = {
         open_fn = require('packer.util').float,
     }
-}})
+} })
